0f06e4adf022244d847339519dbf3c38b9713e06
betzalt <betzalt@icloud.com>
2015-11-30 20:43:00 -0600

    refactor ResultMedia

--- src/components/YTResult.js
+++ src/components/YTResult.js
@@ -4,14 +4,68 @@ import YTResultMedia from './YTResultMedia';
 
 export default class YTResult extends React.Component {
   static propTypes = {
-    id: React.PropTypes.string.isRequired,
-    title: React.PropTypes.string.isRequired,
-    channelTitle: React.PropTypes.string.isRequired,
-    thumbnail: React.PropTypes.string.isRequired,
+    id: React.PropTypes.string,
+    title: React.PropTypes.string,
+    channelTitle: React.PropTypes.string,
+    thumbnail: React.PropTypes.string,
+    isPlaceholder: React.PropTypes.bool,
+  };
+
+  static defaultProps = {
+    isPlaceholder: false,
   };
 
   render() {
-    const { id, title, channelTitle, thumbnail } = this.props;
+    const { id, title, channelTitle, thumbnail, isPlaceholder } = this.props;
+
+    const mediaPlaceholder = (
+      <div style={{
+        paddingTop: '75%',
+        backgroundColor: MUI.Styles.Colors.grey300,
+        zIndex: 0,
+      }} />
+    );
+
+    const titlePlaceholder = (
+      <div style={{
+        width: '80%',
+        height: '20px',
+        background: MUI.Styles.Colors.grey300,
+      }} />
+    );
+
+    const subtitlePlaceholder = (
+      // shit attempt at simulating spacing on top of text
+      <div style={{
+        width: '50%',
+        paddingTop: '4px',
+        boxSizing: 'border-box',
+      }}>
+
+        <div style={{
+          height: '15px',
+          background: MUI.Styles.Colors.grey300,
+        }} />
+      </div>
+    );
+
+    if (isPlaceholder) {
+      return (
+        <MUI.Card style={{width: '100%', marginTop: '20px'}}>
+          <MUI.CardHeader
+            title={titlePlaceholder}
+            subtitle={subtitlePlaceholder}
+            textStyle={{width: '100%'}}
+            avatar={<MUI.Avatar style={{display: 'none'}} />} // cant disable this shit
+          />
+
+          <MUI.CardMedia>
+            {mediaPlaceholder}
+          </MUI.CardMedia>
+        </MUI.Card>
+      );
+    }
+
     return (
       <MUI.Card style={{width: '100%', marginTop: '20px'}}>
         <MUI.CardHeader
@@ -25,11 +79,14 @@ export default class YTResult extends React.Component {
           }
         />
 
-        <YTResultMedia
-          id={id}
-          title={title}
-          thumbnail={thumbnail}
-        />
+        <MUI.CardMedia>
+          {mediaPlaceholder}
+          <YTResultMedia
+            id={id}
+            title={title}
+            thumbnail={thumbnail}
+          />
+        </MUI.CardMedia>
       </MUI.Card>
     );
   }
--- src/components/YTResultMedia.js
+++ src/components/YTResultMedia.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import MUI from 'material-ui';
-import View from 'react-flexbox';
 import YouTube from 'react-youtube';
 
 export default class YTResultMedia extends React.Component {
@@ -18,57 +16,50 @@ export default class YTResultMedia extends React.Component {
     };
   }
 
-  renderVideo() {
-    if (this.state.loaded) {
-      return (
-        <View style={{
-          position: 'absolute',
-          top: '0',
-          left: '0',
-          width: '100%',
-          height: '100%',
-        }}>
-          <YouTube
-            url={`https://youtube.com/watch?v=${this.props.id}`}
-            opts={{
-              width: '100%',
-              height: '100%',
-              playerVars: {
-                autoplay: 1,
-              },
-            }}
-          />
-        </View>
-      );
-    }
-  }
+  render() {
+    const coverStyle = {
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '100%',
+    };
 
-  renderThumbnail() {
-    return (
+    const thumbnail = (
       <img
         src={this.props.thumbnail}
         alt={`${this.props.title} thumbnail`}
         style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
+          ...coverStyle,
+          zIndex: 1,
         }}
+        onClick={() => this.setState({loaded: true})}
       />
     );
-  }
 
-  render() {
+    const video = (
+      <div style={{
+        ...coverStyle,
+        zIndex: 2,
+      }}>
+        <YouTube
+          url={`https://youtube.com/watch?v=${this.props.id}`}
+          opts={{
+            width: '100%',
+            height: '100%',
+            playerVars: {
+              autoplay: 1,
+            },
+          }}
+        />
+      </div>
+    );
+
     return (
-      <MUI.CardMedia onClick={() => this.setState({loaded: true})}>
-        <View style={{
-          display: 'block',
-          position: 'relative',
-          width: '100%',
-          paddingTop: '75%',
-          backgroundColor: MUI.Styles.Colors.grey300,
-        }} />
-        {this.state.loaded ? this.renderVideo() : this.renderThumbnail()}
-      </MUI.CardMedia>
+      <div>
+        {thumbnail}
+        {this.state.loaded ? video : null}
+      </div>
     );
   }
 }
